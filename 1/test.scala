bda0818ee43bcdc271ec0531472104502e705f55
EC2 Default User <ec2-user@ip-172-26-11-114.eu-west-2.compute.internal>
2023-09-16 10:03:20 +0000

    Update test.scala at 2023-09-16 10:03:20.757705 through application.

--- 1/test.scala
+++ 1/test.scala
@@ -1,8 +1,8 @@
+import spark.implicits._
 val spark:SparkSession = SparkSession.builder()
    .master("local[1]").appName("SparkByExamples.com")
    .getOrCreate()
 
-import spark.implicits._
 val columns = Seq("language","users_count")
 val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
 
