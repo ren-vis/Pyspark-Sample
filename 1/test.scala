d16299d56792fd7818bf45a80d38908a5713713f
EC2 Default User <ec2-user@ip-172-26-11-114.eu-west-2.compute.internal>
2023-09-16 10:03:11 +0000

    Update test.scala at 2023-09-16 10:03:11.666305 through application.

--- 1/test.scala
+++ 1/test.scala
@@ -1,14 +1,11 @@
-import org.apache.spark.sql.SparkSession
-val spark:SparkSession = SparkSession.builder().master("local[1]").appName("SparkByExamples.com").getOrCreate()   
+val spark:SparkSession = SparkSession.builder()
+   .master("local[1]").appName("SparkByExamples.com")
+   .getOrCreate()
 
-val data = Seq(('James','','Smith','1991-04-01','M',3000),
-  ('Michael','Rose','','2000-05-19','M',4000),
-  ('Robert','','Williams','1978-09-05','M',4000),
-  ('Maria','Anne','Jones','1967-12-01','F',4000),
-  ('Jen','Mary','Brown','1980-02-17','F',-1)
-)
+import spark.implicits._
+val columns = Seq("language","users_count")
+val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
 
-val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
-val df = spark.createDataFrame(data, schema = columns).toDF(columns:_*)
 
-df.show() 
+var dfFromData2 = spark.createDataFrame(data).toDF(columns:_*)
+dfFromData2.show()
